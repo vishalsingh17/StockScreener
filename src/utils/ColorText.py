6a249ff80c2c39414a578632474795f680f711d6
vishalsingh17 <vishal170997@gmail.com>
2024-09-15 12:12:13 +0530

    Create ColorText.py

---
+++ src/utils/ColorText.py
@@ -0,0 +1,11 @@
+
+# Decoration Class
+class colorText:
+	HEAD = '\033[95m'
+	BLUE = '\033[94m'
+	GREEN = '\033[92m'
+	WARN = '\033[93m'
+	FAIL = '\033[91m'
+	END = '\033[0m'
+	BOLD = '\033[1m'
+	UNDR = '\033[4m'
